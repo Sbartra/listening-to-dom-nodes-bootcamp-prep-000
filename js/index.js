9a018167ee253696044b3fac65ef2f8d6ba43883
Segundo Bartra <segundobartra@gmail.com>
2019-03-07 00:45:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ main.addEventListener('click', function(event){
 });
 
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.key);
 });
 
 const input = document.querySelector('input')
- 
+
 input.addEventListener('keydown', function(e) {
   if (e.key === "g") {
     return e.preventDefault()
@@ -22,25 +22,25 @@ input.addEventListener('keydown', function(e) {
 });
 
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
 }
 
 const divs = document.querySelectorAll('div');
- 
+
 function bubble(e) {
   // stop! that! propagation!
   e.stopPropagation();
- 
+
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
